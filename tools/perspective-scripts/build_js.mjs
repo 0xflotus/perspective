0c7c6da391812d51086b8023407fd5c3a2bc6143
Andrew Stein <steinlink@gmail.com>
2023-06-25 14:58:11 -0400

    Add `@finos/perspective-scripts` tools package

--- tools/perspective-scripts/build_js.mjs
+++ tools/perspective-scripts/build_js.mjs
@@ -1,17 +1,12 @@
 /******************************************************************************
  *
- * Copyright (c) 2019, the Perspective Authors.
+ * Copyright (c) 2017, the Perspective Authors.
  *
  * This file is part of the Perspective library, distributed under the terms of
  * the Apache License 2.0.  The full license can be found in the LICENSE file.
  *
  */
 
-import { kill_jlab } from "./jlab_start.mjs";
+import { run_with_scope } from "./sh_perspective.mjs";
 
-async function globalTeardown() {
-    console.log("Reached globalTeardown.js");
-    await kill_jlab();
-}
-
-export default globalTeardown;
+await run_with_scope`build`;
